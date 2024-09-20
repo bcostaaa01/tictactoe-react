469ad7d7686906c2d48dfedbace14eed18e8c172
Bruno <bcostinha01@gmail.com>
2024-09-20 14:21:23 +0000

    feat: allow for user to edit the player details dynamically

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
+
 export function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <li>
-      <span className="player">
-        <span className="player-name">{name}</span>
-        <span className="player-symnol">{symbol}</span>
-      </span>
-      <button>Edit</button>
+      {isEditing ? (
+        <span className="player">
+          <input className="player-name" defaultValue={name} />
+          <input className="player-symnol" defaultValue={symbol} />
+        </span>
+      ) : (
+        <span className="player">
+          <span className="player-name">{name}</span>
+          <span className="player-symnol">{symbol}</span>
+        </span>
+      )}
+      <button onClick={() => setIsEditing(!isEditing)}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
